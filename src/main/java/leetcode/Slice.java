f84f94c29bb377ae31c8fb5706c097ecd3b1dd51
comyxy <comyxy@foxmail.com>
2021-07-26 22:05:23 +0800

    lc problems

--- src/main/java/leetcode/Slice.java
+++ src/main/java/leetcode/Slice.java
@@ -68,7 +68,7 @@ public class Slice {
                 }
 
                 if (nums[second] + nums[third] == target) {
-                    res.add(new ArrayList<>(Arrays.asList(nums[first],nums[second], nums[third])));
+                    res.add(new ArrayList<>(Arrays.asList(nums[first], nums[second], nums[third])));
                 }
             }
         }
@@ -221,16 +221,137 @@ public class Slice {
         int[] diff = new int[52];
         for (int[] range : ranges) {
             diff[range[0]]++;
-            diff[range[1]+1]--;
+            diff[range[1] + 1]--;
         }
         int cur = 0;
         for (int i = 1; i <= 50; i++) {
             cur += diff[i];
-            if(i >= left && i <= right && cur < 1) {
+            if (i >= left && i <= right && cur < 1) {
                 return false;
             }
         }
         return true;
+    }
+
+    /**
+     * https://leetcode-cn.com/problems/longest-common-subsequence/solution/zui-chang-gong-gong-zi-xu-lie-by-leetcod-y7u0/
+     * LCS O(m*n)
+     */
+    public int longestCommonSubsequence(String text1, String text2) {
+        final int m = text1.length(), n = text2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    /**
+     * https://leetcode-cn.com/problems/longest-increasing-subsequence/solution/zui-chang-shang-sheng-zi-xu-lie-by-leetcode-soluti/
+     * dp O(n*n)
+     */
+    public int lengthOfLIS(int[] nums) {
+        final int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] dp = new int[n];
+        int maxn = 1;
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+                maxn = Math.max(dp[i], maxn);
+            }
+        }
+        return maxn;
+    }
+
+    /**
+     * greedy + binary search
+     */
+    public int lengthOfLISV2(int[] nums) {
+        final int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] d = new int[n];
+        d[0] = nums[0];
+        int len = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > d[len - 1]) {
+                d[len++] = nums[i];
+            } else {
+                // 查找左边界的二分
+                int lo = 0, hi = len - 1;
+                while (lo <= hi) {
+                    int mid = (lo + hi) >> 1;
+                    if (d[mid] >= nums[i]) {
+                        hi = mid - 1;
+                    } else {
+                        lo = mid + 1;
+                    }
+                }
+                int pos = 0;
+                if(lo >= len) {
+                    throw new IllegalStateException();
+                }else {
+                    pos = lo;
+                }
+                d[pos] = nums[i];
+            }
+        }
+        return len;
+    }
+
+    /**
+     * https://leetcode-cn.com/problems/minimum-operations-to-make-a-subsequence/
+     */
+    public int minOperations(int[] target, int[] arr) {
+        final int n = target.length, m = arr.length;
+        Map<Integer, Integer> value2idx = new HashMap<>(n);
+        for (int i = 0; i < n; i++) {
+            value2idx.put(target[i], i);
+        }
+        List<Integer> nA = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            if(value2idx.containsKey(arr[i])) {
+                nA.add(value2idx.get(arr[i]));
+            }
+        }
+        //
+        final int l = nA.size();
+        if(l == 0) {
+            return n;
+        }
+        int[] d = new int[l];
+        d[0] = nA.get(0);
+        int len = 1;
+        for (int i = 1; i < l; i++) {
+            if(nA.get(i) > d[len - 1]) {
+                d[len++] = nA.get(i);
+            }else {
+                int lo = 0, hi = len - 1;
+                while (lo <= hi) {
+                    int mid = (lo + hi) >>> 1;
+                    if(d[mid] >= nA.get(i)) {
+                        hi = mid - 1;
+                    }else {
+                        lo = mid + 1;
+                    }
+                }
+                d[lo] = nA.get(i);
+            }
+        }
+        return n - len;
     }
 
 }
--- src/test/java/leetcode/SliceTest.java
+++ src/test/java/leetcode/SliceTest.java
@@ -25,4 +25,30 @@ class SliceTest {
         assertEquals(3, slice.leastBricks(ll));
     }
 
+    @Test
+    void longestCommonSubsequence() {
+        String t1 = "abcde";
+        String t2 = "ace";
+
+        assertEquals(3, slice.longestCommonSubsequence(t1, t2));
+    }
+
+    @Test
+    void lengthOfLIS() {
+        int[] nums = new int[]{10,9,2,5,3,7,101,18};
+        assertEquals(4, slice.lengthOfLIS(nums));
+    }
+
+    @Test
+    void lengthOfLISV2() {
+        int[] nums = new int[]{0,1,0,3,2,3};
+        assertEquals(4, slice.lengthOfLISV2(nums));
+    }
+
+    @Test
+    void minOperations() {
+        int[] target = new int[]{6,4,8,1,3,2};
+        int[] arr = new int[]{4,7,6,2,3,8,6,1};
+        assertEquals(3, slice.minOperations(target, arr));
+    }
 }
